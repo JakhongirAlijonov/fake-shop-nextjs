71f63fc2b7d20b1adc1861edd7dd80f0f706c557
JakhongirAlijonov <jakhongirweb@gmail.com>
2023-09-11 12:57:58 +0500

    images optimized

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 function Navbar() {
   return (
-    <header className="text-gray-400 bg-gray-900 body-font  fixed top-0 w-full">
+    <header className="text-gray-400 bg-gray-900 body-font   fixed top-0 w-full z-10">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <Link href="/" className="flex cursor-pointer title-font font-medium items-center text-white mb-4 md:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
@@ -16,9 +16,9 @@ function Navbar() {
        
       </nav>
 
-      <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Login
+      <button className="inline-flex items-center  bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700  rounded text-base mt-4 md:mt-0">Login
       
-    </button> <button className="ml-2 inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Sign up
+    </button> <button className="ml-2 inline-flex   items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Sign up
       
       </button>
     </div>
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,25 +1,31 @@
+"use client";
+import { ProductType } from "@/interface";
+import Image from "next/image";
+import Link from "next/link";
+import React, { FC } from "react";
+import CustomImage from "./Image";
 
-"use client"
-import { ProductType } from '@/interface'
-import React, { FC } from 'react'
-
-const  Product: FC<{product: ProductType}>= ({product})=> {
-    
-    
+const Product: FC<{ product: ProductType }> = ({ product }) => {
   return (
     <div>
-        <div className="bg-gray-700 p-6  rounded-lg ">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" src={product.image} alt={product.title}/>
-          <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">{product.category}</h3>
-         <div className="font-semibold  flex items-center justfy-between mt-4 mb-1">
-            <h2 className="text-lg truncate w-44">{product.title}</h2>
-         <h3 className="ml-5">${product.price}</h3>
-         </div>
-       
-          <p className="leading-relaxed text-base line-clamp-3">{product.description}</p>
+      <Link  href={`/product/${product.id}`} className="bg-white text-gray-700 p-6 h-96 flex flex-col   rounded-lg group hover:scale-105 transition-transform ease-out duration-200  ">
+        <div className="relative max-h-80 flex-1 ">
+          <CustomImage product={product} fill  />
         </div>
-    </div>
-  )
-}
+        <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">
+          {product.category}
+        </h3>
+        <div className="font-semibold  flex items-center justfy-between mt-4 mb-1">
+          <h2 className="text-lg truncate w-44">{product.title}</h2>
+          <h3 className="ml-5">${product.price}</h3>
+        </div>
 
-export default Product
+        <p className="leading-relaxed text-base line-clamp-3">
+          {product.description}
+        </p>
+      </Link>
+    </div>
+  );
+};
+
+export default Product;
